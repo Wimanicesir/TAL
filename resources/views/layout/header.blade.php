0a565f85db01c6ef891baaf443f6b9d7484e9712
Tim Van Meirvenne <tim.vanme@wax.be>
2018-07-21 16:46:03 +0200

    Initial commit

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,29 @@
+<style type="text/css">
+    @font-face {
+        font-family: header;
+        src: url('{{ public_path('fonts/Avenida.tff') }}');
+    }
+</style>
+
+<header class="header row">
+    <div class="col-sm-4 col-md-4 col-lg-4">
+        <div class="header__left">
+            <span class="header__left__logo">The Animation Library</span>
+        </div>
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 floatRight">
+        <div class="header__right">
+            <nav class="header__right__nav">
+                <a href="#">Home</a>
+                <a href="#">Shop</a>
+                <a href="#">Freebies</a>
+                <a href="#">Tips & Tricks</a>
+                <a href="#">Contact</a>
+                <span class="header__right__cart">Cart</span>
+            </nav>
+
+        </div>
+    </div>
+</header>
+
+
